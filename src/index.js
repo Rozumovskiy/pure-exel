390efe12a0df88b2a1bbe6bb6d899e54356edb53
bogdan <bogdanrozumovskij@gmail.com>
2020-05-14 21:30:26 +0300

    Finish base framework funtional

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
 import {Excel} from '@/components/excel/Excel';
-import {Header} from './components/header/Header';
-import {Toolbar} from './components/toolbar/Toolbar';
-import {Formula} from './components/formula/Formula';
-import {Table} from './components/table/Table';
+import {Header} from '@/components/header/Header';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
 import './scss/index.scss';
 
 const excel = new Excel('#app', {
