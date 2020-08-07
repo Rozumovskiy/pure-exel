f8d2054816c37bad7cee460e11313a79952373a8
bogdan <bogdanrozumovskij@gmail.com>
2020-08-07 19:11:57 +0300

    Finish redux and table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -14,7 +14,7 @@ class Dom {
     }
 
     text(text) {
-        if (typeof text === 'string') {
+        if (typeof text !== 'undefined') {
             this.$el.textContent = text;
             return this;
         }
@@ -77,6 +77,13 @@ class Dom {
             .forEach(key => this.$el.style[key] = styles[key]);
     }
 
+    getStyles(styles = []) {
+        return styles.reduce((res, s) => {
+            res[s] = this.$el.style[s];
+            return res;
+        }, {})
+    }
+
     id(parse) {
         if (parse) {
            const parsed = this.id().split(':');
@@ -91,6 +98,14 @@ class Dom {
     focus() {
         this.$el.focus();
         return this;
+    }
+
+    attr(name, value) {
+        if (value) {
+            this.$el.setAttribute(name, value);
+            return this;
+        }
+        return this.$el.getAttribute(name);
     }
 
     addClass(className) {
--- src/index.js
+++ src/index.js
@@ -5,9 +5,23 @@ import {Toolbar} from '@/components/toolbar/Toolbar';
 import {Formula} from '@/components/formula/Formula';
 import {Table} from '@/components/table/Table';
 import './scss/index.scss';
+import {createStore} from '@core/createStore';
+import {storage, debounce} from '@core/utils';
+import {rootReducer} from '@/redux/rootReducer';
+import {initialState} from './redux/initialState';
+
+
+const store = createStore(rootReducer, initialState);
+
+const stateListener = debounce(state => {
+    storage('excel-key', state)
+}, 300)
+
+store.subscribe(stateListener);
 
 const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table]
+    components: [Header, Toolbar, Formula, Table],
+    store
 });
 
 excel.render();
